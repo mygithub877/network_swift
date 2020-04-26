1d7f37ce3624c25260ce057f1e7d73734d236b01
LWJ <lwj@xx.cc>
2020-04-26 18:59:19 +0800

    project

---
+++ NetworkKit/NetworkKit/NetworkKit.h
@@ -0,0 +1,19 @@
+//
+//  NetworkKit.h
+//  NetworkKit
+//
+//  Created by wenjie liu on 2020/4/26.
+//  Copyright © 2020 iloc.cc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkKit.
+FOUNDATION_EXPORT double NetworkKitVersionNumber;
+
+//! Project version string for NetworkKit.
+FOUNDATION_EXPORT const unsigned char NetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkKit/PublicHeader.h>
+
+
---
+++ NetworkKit/Pods/Target Support Files/Pods-NetworkKit/Pods-NetworkKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NetworkKit : NSObject
+@end
+@implementation PodsDummy_Pods_NetworkKit
+@end
